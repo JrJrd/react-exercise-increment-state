5790bb9c8cc592752c7389fdd130417f9a0cca67
seaer <jrd.seaer@gmail.com>
2023-09-20 16:35:51 +0300

    react state

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,33 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Component } from "react";
+import Heading from "./components/heading.jsx";
+import Button from "./components/button.jsx";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-        </ul>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      customUnit: 0,
+    };
+  }
+  onClick() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  updateCount(unit) {
+    this.setState({ count: this.state.count + unit });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Heading title={`Count is ${this.state.count}`} />
+          <Button
+            onClick={() => this.updateCount(1)}
+          >+1</Button>
+          <Button
+          onClick={() => this.updateCount(5)}
+          >
+            +5
+          </Button>
+          <Button
+           onClick={() => this.updateCount(10)}
+          >
+            +10
+          </Button>
+          <input type="number" value={this.state.customUnit} onChange={(e) => this.setState({customUnit: e.target.value})}/>
+          <Button >INCR</Button>
+          <Button
+          onClick={() => this.updateCount(-this.state.count)}
+          >
+            reset
+          </Button>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
